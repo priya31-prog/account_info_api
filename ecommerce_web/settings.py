0e8f651456c7416b4055d235f78c042222f337c5
priya31-prog <karthika200103@gmail.com>
2025-03-18 08:10:38 +0530

    debug true

--- ecommerce_web/settings.py
+++ ecommerce_web/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-o-=rndm(x)_h(bo))qz0b^131lbwi2hyytfjt=c$daz-u7)q*y"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # post api call
 # APPEND_SLASH = False
